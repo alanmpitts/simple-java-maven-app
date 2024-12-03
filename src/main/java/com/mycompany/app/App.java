e783214115c2dbbc3015113de9baeb50ba8f0803
Alan Pitts <alanmpitts@gmail.com>
2024-12-03 18:51:37 -0500

    change the MESSAGE string

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,9 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
+    private static final String MESSAGE = """
+            "Never memorize something that you can look up"
+                                         --Albert Einstein""";
 
     public App() {}
 
